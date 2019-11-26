2a810ce9d5fffb69c941a7180d685e4be7c3424b
MichaelNewman <newms226@gmail.com>
2019-11-26 00:59:57 -0700

    stash

--- CarShopAPI/entrypoint.sh
+++ CarShopAPI/entrypoint.sh
@@ -5,7 +5,7 @@ unset BUNDLE_BIN
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /backend/tmp/pids/server.pid
+rm -f /CarShopAPI/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
